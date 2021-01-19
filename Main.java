499fee2e6148385e14e1299b3099109b04a07d9d
AidanBugler <buglaida982@gotvdsb.ca>
2021-01-19 00:27:04 +0000

    Added new methods

--- Main.java
+++ Main.java
@@ -4,27 +4,75 @@ import java.io.FileWriter;
 import java.util.Scanner;
 
 class Main {
+    static int [] scoreArray;
+
+    static String [] addNameMethod(String str[], String userInput)
+    {
+      String [] temp  = new String [str.length +1];
+      for (int i=0;i <str.length +1; i++)
+      {
+        temp [i] = str [i]; 
+      }
+      temp[temp.length-1] = userInput;
+      return temp;
+    }
+
+    static void addScoreMethod (int arr [], int userInput)
+    {
+      int [] temp  = new int [arr.length +1];
+      for (int i=0;i <arr.length +1; i++)
+      {
+        temp [i] = arr [i]; 
+      }
+      temp[temp.length-1] = userInput;
+      
+    }
+
+    static String[] remainderNameSortMethod(String str[], int arrLength)
+  {
+    if (arrLength==0)
+    {
+      return str;
+    }
+    String temp;
+    int intTemp;
+    for (int i = 0; i < arrLength-1; i++) 
+    {
+      if (str[i].compareTo(str[i+1]) > 0) {
+      temp = str[i];
+      str[i] = str[i+1];
+      str[i+1] = temp;
+      intTemp = scoreArray[i];
+      scoreArray[i] = scoreArray[i+1];
+      scoreArray[i+1] = intTemp;
+      }
+    }
+
+  return remainderNameSortMethod(str,arrLength-1);
+  }
+  //static int [] remainderScoreSortMethod(int arr[], int arrlength)
+ // {
+ //   return arr;
+ // }
+
   public static void main(String[] args) throws IOException {
+    Scanner input = new Scanner(System.in);
+    int choice; 
     String[] nameArray; 
-    int [] scoreArray;
     String [] totalFile;
     int index = 0;
-    
-    
     //Int varible to count lines of text
     int lineCounter = 0;
+    String newName;
+    int newScore; 
     //Making the File to read from
     File myFile = new File("ScoreSheet.txt");
     myFile.createNewFile();
-
-    //Putting the names and Scores into the File
-    //FileWriter myWriter = new FileWriter (myFile);
     
-    //myWriter.close();
-
     Scanner myReader = new Scanner (myFile);
     while(myReader.hasNextLine())
     {
+      myReader.nextLine();
       lineCounter = lineCounter + 1;
     }
     myReader.close();
@@ -52,11 +100,39 @@ class Main {
       scoreArray [index] = Integer.parseInt(totalFile[i]);
       index = index +1;
     }
+    
+    
+    //Asking the user if they would like to add any names and scores to the FileWriter
+    System.out.println("Would you like to add a new name and score to the file?");
+    System.out.println("1. yes   2. no");
+    choice = Integer.parseInt(input.nextLine());
+
+    if (choice == 1)
+    {
+      System.out.println("What is the new name?");
+      newName = input.nextLine();
+      System.out.println("What score did they get?");
+      newScore = Integer.parseInt(input.nextLine());
+      System.out.println();
+
+      nameArray = addNameMethod(nameArray, newName);
+      addScoreMethod(scoreArray, newScore);
+
+      
+
+      System.out.println("Would you like to save the scores or add more?");
+      System.out.println("1. Add more  2. Save and move on");
+      choice = Integer.parseInt(input.nextLine());
+    }
+    //Putting the names and Scores into the File
+    //FileWriter myWriter = new FileWriter (myFile);
+    
+    //myWriter.close();
+
+    
     for (int i=0; i< (lineCounter/2); i++ )
     {
-      System.out.println(nameArray[i]);
-      System.out.println(scoreArray[i]);
+    System.out.println(remainderNameSortMethod(nameArray,(lineCounter/2))[i] + " " +scoreArray[i]);
     }
-
   }
 }
